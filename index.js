c0c383ae53a8ad3de609179d1450fb2aaceb75e5
TeguhArdiansyah <teguhardiansyah9921@gmail.com>
2023-09-21 19:50:35 +0700

    update project

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
-    res.write("Hello World!");
+    res.write("Hello Section Jayapura");
     res.end();
 }).listen(port, () => {
     console.log(`Server running on localhost:${port}`);
